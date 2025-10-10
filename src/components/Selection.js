8cbf95ad1eef7b2abd56e42ddae11f7ca6f4e0d9
Lokesh Goswami <lokesh.goswami.2003@gmail.com>
2025-10-10 15:32:17 +0530

    try

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,20 @@
+import React, { useState } from "react";
 
+const Selection = (props) => {
+  const { applyColor } = props;
+
+  // Keeps track of this box’s background color
+  const [selectionStyle, updateSelectionStyle] = useState({ background: "" });
+
+  return (
+    <div
+      className="fix-box"
+      style={selectionStyle}
+      onClick={() => applyColor(updateSelectionStyle)}
+    >
+      <h3 className="subheading">Selection</h3>
+    </div>
+  );
+};
+
+export default Selection;
